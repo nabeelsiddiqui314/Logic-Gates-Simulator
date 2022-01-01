fe55f4a0725095352267b6cd4f64068d4aa9f4b4
Nabeel Siddiqui <nabeel.siddiqui.314@gmail.com>
2022-01-01 23:33:07 +0530

    added sfml

--- Logic-Gates-Simulator/src/main.cpp
+++ Logic-Gates-Simulator/src/main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
 
 int main() {
-	std::cout << "hello world";
-	std::cin.get();
+	sf::RenderWindow window;
+
+	window.create(sf::VideoMode(800, 600), "Logic Gate Simulator");
+
+	while (window.isOpen()) {
+		sf::Event event;
+
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+			case sf::Event::Closed:
+				window.close();
+				break;
+			}
+		}
+
+		window.clear();
+		window.display();
+	}
 }
